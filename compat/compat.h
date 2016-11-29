abb56b11469174c03aa81abce1b3370a96586cd8
Carlos Roberto <carlosrpj555@gmail.com>
2016-11-29 15:37:03 -0200

    Remoção do printf de debug

--- compat/compat.h
+++ compat/compat.h
@@ -45,8 +45,6 @@ void c_gets(String string, int size)
 	// Aloca no máximo o passado por parametro size
 	int size_in = wcslen(buffer) <= size ? wcslen(buffer) : size;
 	
-	printf("tamanho: %d", size_in);
-	
 	// Faz a conversão
 	for(int i=0; i<size_in; i++)
 	{
